1c7bc2c7440ea820cd80ee38612c151da5becdf1
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 12:45:59 +0300

    Updated demo project

--- demo/src/main/java/com/visuality/demo/MainActivity.java
+++ demo/src/main/java/com/visuality/demo/MainActivity.java
@@ -2,12 +2,37 @@ package com.visuality.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.visuality.wordy.effects.CaseEffect;
+import com.visuality.wordy.tools.TextBuilder;
+import com.visuality.wordy.types.TextCase;
 
 public class MainActivity extends Activity {
+
+    private TextView titleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initializeTitleTextView();
+        this.updateTitleTextView(
+                new TextBuilder("wordy demo")
+                    .add(new CaseEffect(TextCase.FIRST_UPPER_NEXT_LOWER))
+                    .build()
+        );
+    }
+
+    private void initializeTitleTextView() {
+        this.titleTextView = this.findViewById(
+                R.id.title_text_view
+        );
+    }
+
+    private void updateTitleTextView(String title) {
+        this.titleTextView.setText(
+                title
+        );
     }
 }
