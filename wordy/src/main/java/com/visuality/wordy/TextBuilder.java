64b1e997be18f86617ce9672c9f29243765d30b7
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 12:17:45 +0300

    Added TextBuilder class

---
+++ wordy/src/main/java/com/visuality/wordy/TextBuilder.java
@@ -0,0 +1,13 @@
+package com.visuality.wordy;
+
+public class TextBuilder {
+
+    private String sourceText;
+
+    public TextBuilder(
+            String sourceText
+    ) {
+        super();
+        this.sourceText = sourceText;
+    }
+}
