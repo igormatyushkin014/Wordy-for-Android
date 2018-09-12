b22e67534e79700ab9300cdec4cbe30ef67b7711
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 17:12:53 +0300

    Segments for TextFilter

--- wordy/src/main/java/com/visuality/wordy/tools/TextFilter.java
+++ wordy/src/main/java/com/visuality/wordy/tools/TextFilter.java
@@ -1,6 +1,7 @@
 package com.visuality.wordy.tools;
 
 import com.visuality.wordy.effects.Effect;
+import com.visuality.wordy.types.TextSegment;
 
 import java.util.ArrayList;
 
@@ -8,7 +9,7 @@ public class TextFilter {
 
     private String sourceText;
 
-    private ArrayList<Effect> effects;
+    private ArrayList<TextChange> changes;
 
     public TextFilter(
             String sourceText
@@ -16,12 +17,39 @@ public class TextFilter {
         super();
 
         this.sourceText = sourceText;
-        this.effects = new ArrayList<>();
+        this.changes = new ArrayList<>();
     }
 
     public TextFilter apply(Effect effect) {
-        this.effects.add(
-                effect
+        TextSegment segment = new TextSegment(
+                0,
+                this.sourceText.length() - 1
+        );
+        TextChange change = new TextChange(
+                effect,
+                segment
+        );
+        this.changes.add(
+                change
+        );
+        return this;
+    }
+
+    public TextFilter apply(
+            Effect effect,
+            int startIndex,
+            int endIndex
+    ) {
+        TextSegment segment = new TextSegment(
+                startIndex,
+                endIndex
+        );
+        TextChange change = new TextChange(
+                effect,
+                segment
+        );
+        this.changes.add(
+                change
         );
         return this;
     }
@@ -29,12 +57,55 @@ public class TextFilter {
     public String getResult() {
         String resultText = new String(this.sourceText);
 
-        for (Effect effect : this.effects) {
-            resultText = effect.getFilteredText(
+        for (TextChange change : this.changes) {
+            SegmentFinder segmentFinder = new SegmentFinder(
                     resultText
             );
+
+            String substringBeforeSegment = segmentFinder.getSubstringBeforeSegment(
+                    change.getSegment()
+            );
+            String substringFromSegment = segmentFinder.getSubstringFromSegment(
+                    change.getSegment()
+            );
+            String substringAfterSegment = segmentFinder.getSubstringAfterSegment(
+                    change.getSegment()
+            );
+
+            String filteredSubstringFromSegment = change.getEffect().getFilteredText(
+                    substringFromSegment
+            );
+
+            resultText = substringBeforeSegment
+                    + filteredSubstringFromSegment
+                    + substringAfterSegment;
         }
 
         return resultText;
+    }
+
+    private class TextChange {
+
+        private Effect effect;
+
+        public Effect getEffect() {
+            return this.effect;
+        }
+
+        private TextSegment segment;
+
+        public TextSegment getSegment() {
+            return this.segment;
+        }
+
+        public TextChange(
+                Effect effect,
+                TextSegment segment
+        ) {
+            super();
+
+            this.effect = effect;
+            this.segment = segment;
+        }
     }
 }
