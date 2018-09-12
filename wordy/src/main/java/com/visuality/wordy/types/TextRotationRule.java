6b6f9a4dc4ee6b3ea12d5ba25831f7fcdb704038
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 13:39:29 +0300

    Implemented TextRotationRule class

---
+++ wordy/src/main/java/com/visuality/wordy/types/TextRotationRule.java
@@ -0,0 +1,54 @@
+package com.visuality.wordy.types;
+
+public class TextRotationRule {
+
+    private String normal;
+
+    public String getNormal() {
+        return this.normal;
+    }
+
+    private String rotated;
+
+    public String getRotated() {
+        return this.rotated;
+    }
+
+    private TextRotationRule(
+            String normal,
+            String rotated
+    ) {
+        super();
+
+        this.normal = normal;
+        this.rotated = rotated;
+    }
+
+    public static class Builder {
+
+        private String normal;
+
+        public Builder setNormal(String normal) {
+            this.normal = normal;
+            return this;
+        }
+
+        private String rotated;
+
+        public Builder setRotated(String rotated) {
+            this.rotated = rotated;
+            return this;
+        }
+
+        public Builder() {
+            super();
+        }
+
+        public TextRotationRule build() {
+            return new TextRotationRule(
+                    this.normal,
+                    this.rotated
+            );
+        }
+    }
+}
