11572d5d9a696c7a6691dbb1b210ee82285cee64
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 12:22:27 +0300

    Implemented effect collection in TextBuilder class

--- wordy/src/main/java/com/visuality/wordy/tools/TextBuilder.java
+++ wordy/src/main/java/com/visuality/wordy/tools/TextBuilder.java
@@ -1,13 +1,28 @@
 package com.visuality.wordy.tools;
 
+import com.visuality.wordy.effects.Effect;
+
+import java.util.ArrayList;
+
 public class TextBuilder {
 
     private String sourceText;
+
+    private ArrayList<Effect> effects;
 
     public TextBuilder(
             String sourceText
     ) {
         super();
+
         this.sourceText = sourceText;
+        this.effects = new ArrayList<>();
+    }
+
+    public TextBuilder add(Effect effect) {
+        this.effects.add(
+                effect
+        );
+        return this;
     }
 }
