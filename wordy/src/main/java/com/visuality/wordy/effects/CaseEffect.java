dd23c8d45427c456f2057ddb4f8525574b71280b
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 12:39:37 +0300

    Implemented cases for CaseEffect class

--- wordy/src/main/java/com/visuality/wordy/effects/CaseEffect.java
+++ wordy/src/main/java/com/visuality/wordy/effects/CaseEffect.java
@@ -2,12 +2,55 @@ package com.visuality.wordy.effects;
 
 public class CaseEffect extends Effect {
 
-    public CaseEffect() {
+    private CaseChange caseChange;
+
+    public CaseEffect(CaseChange caseChange) {
         super();
+        this.caseChange = caseChange;
     }
 
     @Override
     public String getFilteredText(String sourceText) {
+        switch (this.caseChange) {
+            case ALL_UPPER:
+                return sourceText.toUpperCase();
+            case ALL_LOWER:
+                return sourceText.toLowerCase();
+            case FIRST_UPPER_NEXT_LOWER:
+                if (sourceText.isEmpty()) {
+                    return "";
+                } else {
+                    String firstLetter = sourceText.substring(0, 1);
+                    String otherText = sourceText.substring(1);
+
+                    return String.format(
+                            "%s%s",
+                            firstLetter.toUpperCase(),
+                            otherText.toLowerCase()
+                    );
+                }
+            case FIRST_LOWER_NEXT_UPPER:
+                if (sourceText.isEmpty()) {
+                    return "";
+                } else {
+                    String firstLetter = sourceText.substring(0, 1);
+                    String otherText = sourceText.substring(1);
+
+                    return String.format(
+                            "%s%s",
+                            firstLetter.toLowerCase(),
+                            otherText.toUpperCase()
+                    );
+                }
+        }
+
         return sourceText;
+    }
+
+    public enum CaseChange {
+        ALL_UPPER,
+        ALL_LOWER,
+        FIRST_UPPER_NEXT_LOWER,
+        FIRST_LOWER_NEXT_UPPER
     }
 }
