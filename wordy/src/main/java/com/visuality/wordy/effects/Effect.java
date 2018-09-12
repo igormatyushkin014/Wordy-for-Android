397fac2dae65800b4a77696171837a3ad609d2a6
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 15:37:54 +0300

    Updated Effect class

--- wordy/src/main/java/com/visuality/wordy/effects/Effect.java
+++ wordy/src/main/java/com/visuality/wordy/effects/Effect.java
@@ -2,5 +2,9 @@ package com.visuality.wordy.effects;
 
 public abstract class Effect {
 
+    public Effect() {
+        super();
+    }
+
     public abstract String getFilteredText(String sourceText);
 }
