b3f27cb1721f064fe8598097337dcc1e8caf3cbf
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 12:56:16 +0300

    Added RotationEffect

---
+++ wordy/src/main/java/com/visuality/wordy/effects/RotationEffect.java
@@ -0,0 +1,31 @@
+package com.visuality.wordy.effects;
+
+import com.visuality.wordy.types.TextRotation;
+
+class RotationEffect extends Effect {
+
+    private TextRotation textRotation;
+
+    public RotationEffect(
+            TextRotation textRotation
+    ) {
+        super();
+        this.textRotation = textRotation;
+    }
+
+    @Override
+    public String getFilteredText(String sourceText) {
+        // TODO: Implement method.
+
+        switch (textRotation) {
+            case NORMAL:
+                return new String(sourceText);
+            case UPSIDE_DOWN:
+                return new String(sourceText);
+            case INVERTED:
+                return new String(sourceText);
+            default:
+                return new String(sourceText);
+        }
+    }
+}
