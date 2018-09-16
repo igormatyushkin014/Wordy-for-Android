cf750e699281db538a75dab7379bbd9d7f61b641
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-17 02:52:10 +0300

    Implemented TransliterationManager class

--- wordy/src/main/java/com/visuality/wordy/tools/TransliterationManager.java
+++ wordy/src/main/java/com/visuality/wordy/tools/TransliterationManager.java
@@ -1,6 +1,10 @@
 package com.visuality.wordy.tools;
 
+import com.visuality.wordy.data.transliteration.TransliterationRule;
+import com.visuality.wordy.data.transliteration.TransliterationStore;
+import com.visuality.wordy.effects.CaseEffect;
 import com.visuality.wordy.types.Language;
+import com.visuality.wordy.types.TextCase;
 
 public class TransliterationManager {
 
@@ -27,6 +31,41 @@ public class TransliterationManager {
     }
 
     public String getText(String sourceText) {
-        return "";
+        TransliterationRule[] rules = new TransliterationStore().getRules(
+                this.sourceLanguage,
+                this.targetLanguage
+        );
+
+        String resultText = new String(sourceText);
+
+        for (TransliterationRule rule : rules) {
+            resultText = resultText.replace(
+                    new EffectManager(rule.getReplaceable())
+                            .apply(new CaseEffect(TextCase.FIRST_UPPER_NEXT_LOWER))
+                            .getResult(),
+                    new EffectManager(rule.getReplacement())
+                            .apply(new CaseEffect(TextCase.FIRST_UPPER_NEXT_LOWER))
+                            .getResult()
+            );
+
+            resultText = resultText.replace(
+                    rule.getReplaceable().toUpperCase(),
+                    new EffectManager(rule.getReplacement())
+                            .apply(new CaseEffect(TextCase.FIRST_UPPER_NEXT_LOWER))
+                            .getResult()
+            );
+
+            resultText = resultText.replace(
+                    rule.getReplaceable().toUpperCase(),
+                    rule.getReplacement().toUpperCase()
+            );
+
+            resultText = resultText.replace(
+                    rule.getReplaceable(),
+                    rule.getReplacement()
+            );
+        }
+
+        return resultText;
     }
 }
