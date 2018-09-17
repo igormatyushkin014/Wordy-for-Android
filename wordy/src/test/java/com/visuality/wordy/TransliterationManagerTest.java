d46dec60dd8251bc47e503e9039bb17a33598fe9
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-17 03:33:06 +0300

    Updated tests for TransliterationManager

--- wordy/src/test/java/com/visuality/wordy/TransliterationManagerTest.java
+++ wordy/src/test/java/com/visuality/wordy/TransliterationManagerTest.java
@@ -10,8 +10,8 @@ public class TransliterationManagerTest {
 
     @Test
     public void transliterationManager_fromEnglishToRussian_isCorrect() {
-        String sourceText = "This IS a test";
-        String assertion = "Тхис ИС а тэст";
+        String sourceText = "Actually, it Was really hard to implement such a thing like this Library, BUT finally it got implemented.";
+        String assertion = "Эктаоли, ит Вос рилли хэрд то имплэмэнт сач э тинг ликэ тис Либрэри, БАТ финоли ит гот имплэмэнтэд.";
         String result = new TransliterationManager(Language.ENGLISH, Language.RUSSIAN)
                 .getText(sourceText);
         Assert.assertTrue(
